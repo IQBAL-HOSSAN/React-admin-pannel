a0cae367d8c9deea675ba3444bf6b76dcc6dd89f
Iqbal Hossan <engiqbal1999@gmail.com>
2022-05-20 16:52:03 +0600

    created charts component with react-circular-progress and recharts and display after the widget component on the home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,3 +1,5 @@
+import Chart from "../../components/chart/Chart";
+import Featured from "../../components/featured/Featured";
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Widget from "../../components/widget/Widget";
@@ -9,11 +11,17 @@ const Home = () => {
       <Sidebar />
       <div className="homeContainer">
         <Navbar />
+        {/* Widget component */}
         <div className="widgets">
           <Widget type="user" />
           <Widget type="order" />
           <Widget type="earning" />
           <Widget type="balance" />
+        </div>
+        {/* Chart component */}
+        <div className="charts">
+          <Featured />
+          <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} />
         </div>
       </div>
     </section>
