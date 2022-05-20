a45eb93bb41792101d05f9eb1b17ef34a9a1ed16
Iqbal Hossan <engiqbal1999@gmail.com>
2022-05-20 15:08:45 +0600

    created the navbar component and display on the top on home pag

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,3 +1,4 @@
+import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "./home.scss";
 
@@ -5,7 +6,9 @@ const Home = () => {
   return (
     <section className="home">
       <Sidebar />
-      <div className="homeContainer"></div>
+      <div className="homeContainer">
+        <Navbar />
+      </div>
     </section>
   );
 };
